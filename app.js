d181d6677cffb14543c6e8ee8ee83a56283783e6
OroDev <oro91dev@gmail.com>
2018-10-10 09:44:09 +0200

    second
    Added scoreboeard stats and name

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-const title = 'My First React Element!';
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Player 1</span>
+        </header>
+    );
+}
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Olav
+            </span>
 
-const desc =  'I just learned how to create a React node and render it into the DOM.';
-const myTitleID = 'main-title';
-const name = 'Olav';
-const header = (
-    <header>
-        <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-        <p>{ desc }</p>
-    </header>
-);
+            <Counter />
+        </div>
+    );
+}
 
+const Counter = () => {
+    return (
+    <div className="counter">
+        <button className="counter-action decrement">-</button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment">+</button>
+    </div>
+    );
+
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+         <Header />
+
+         <Player />
+        </div>
+    );
+}
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
